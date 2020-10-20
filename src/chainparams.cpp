5a25b6821a7723358eb48f08f98db6ce68743ade
5G <53220766+5G-Cash@users.noreply.github.com>
2020-10-20 15:47:08 -0400

    Block time Correction

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -91,7 +91,7 @@ public:
         consensus.powLimit = uint256S("00000fffffffffffffffffffffffffffffffffffffffffffffffffffffffffff");
         //static const int64 nInterval = nTargetTimespan / nTargetSpacing;
         consensus.nPowTargetTimespan = 20 * 60; // 20 minutes between retargets 
-        consensus.nPowTargetSpacing = 5 * 60; // alternate PoW/PoS every five minutes
+        consensus.nPowTargetSpacing = 10 * 60; // alternate PoW/PoS every five minutes
         consensus.nDgwPastBlocks = 30; // number of blocks to average in Dark Gravity Wave
         consensus.fPowAllowMinDifficultyBlocks = false;
         consensus.fPowNoRetargeting = false;
