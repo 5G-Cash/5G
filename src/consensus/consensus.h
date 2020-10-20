e89c1a8f4b1a0d2e56aa109e6ada941ccd1901bb
5G <53220766+5G-Cash@users.noreply.github.com>
2020-10-20 18:37:12 -0400

    Block height upgrade for POS and MN start

--- src/consensus/consensus.h
+++ src/consensus/consensus.h
@@ -11,8 +11,8 @@
 #include <stdint.h>
 
 // HF constants
-static const int HF_FIVEGNODE_HEIGHT = 2200;
-static const int HF_FIVEGNODE_PAYMENT_START = HF_FIVEGNODE_HEIGHT + 1; // after 2200 it starts
+static const int HF_FIVEGNODE_HEIGHT = 2350;
+static const int HF_FIVEGNODE_PAYMENT_START = HF_FIVEGNODE_HEIGHT + 1; // after 2350 it starts
 
 
 /** The maximum allowed size for a serialized block, in bytes (only for buffer size limits) */
