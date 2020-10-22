dfff7678652bc93674e9b71b2987d90d47e519f6
5G <53220766+5G-Cash@users.noreply.github.com>
2020-10-21 21:34:51 -0400

    nYearBlocksinmin adjusted

--- src/main.cpp
+++ src/main.cpp
@@ -2105,8 +2105,8 @@ CAmount GetBlockSubsidy(int nHeight, const Consensus::Params &consensusParams, i
     bool fPremineBlock = nHeight ==2;
     int nYearBlocksinmin = 105120;
     bool phaseinitialdiff = nHeight > 2 && nHeight <= 1500;
-    bool preparationforpos = nHeight > 1500 && nHeight <= 5000;
-    bool phaseyear1 = nHeight > 5000 && nHeight <= nYearBlocksinmin;
+    bool preparationforpos = nHeight > 1500 && nHeight <= 2500;
+    bool phaseyear1 = nHeight > 2500 && nHeight <= nYearBlocksinmin;
     bool phaseyear2 = nHeight > nYearBlocksinmin && nHeight <= (nYearBlocksinmin * 2);
     bool phaseyear3 = nHeight > (nYearBlocksinmin * 2) && nHeight <= (nYearBlocksinmin * 3);
     bool phaseyear4 = nHeight > (nYearBlocksinmin * 3) && nHeight <= (nYearBlocksinmin * 4);
