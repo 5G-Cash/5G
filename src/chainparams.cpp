c4efa723998227ea95a1d3fdd05575c0e80132af
5G <53220766+5G-Cash@users.noreply.github.com>
2022-05-05 16:49:10 -0400

    //comment update

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -1,6 +1,6 @@
 // Copyright (c) 2010 Satoshi Nakamoto
 // Copyright (c) 2009-2015 The Bitcoin Core developers
-// Copyright (c) 2020 The FivegX Project developers
+// Copyright (c) 2020 The Fiveg Project developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
