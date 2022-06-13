3660f62e87195edd29643cbb9e160628bbbe4708
5G <53220766+5G-Cash@users.noreply.github.com>
2022-06-13 03:27:58 -0400

    Link update

--- contrib/gitian-build.sh
+++ contrib/gitian-build.sh
@@ -20,7 +20,7 @@ osx=true
 SIGNER=
 VERSION=
 commit=false
-url=${url:-https://github.com/FivegXProject/Fiveg}
+url=${url:-https://github.com/5G-Cash/5G/}
 gsigsUrl=https://github.com/bitcoin-core/gitian.sigs
 detachUrl=https://github.com/bitcoin-core/bitcoin-detached-sigs.git
 proc=2
@@ -44,7 +44,7 @@ version		Version number, commit, or branch to build. If building a commit or bra
 
 Options:
 -c|--commit	Indicate that the version argument is for a commit or branch
--u|--url	Specify the URL of the FivegXProject-Fiveg official repository. Default is https://github.com/FivegXProject/Fiveg.git
+-u|--url	Specify the URL of the Fiveg official repository. Default is https://github.com/5G-Cash/5G.git
 -g|--gsigsUrl	Specify the URL of the gitian.sigs repository. Default is https://github.com/bitcoin-core/gitian.sigs
 -d|--detachUrl	Specify the URL of the bitcoin-detached-sigs repository. Default is https://github.com/bitcoin-core/bitcoin-detached-sigs
 -v|--verify 	Verify the Gitian build
