0d84fecd952383c12d9991500aa654ca688d6f9b
alamshafil <alamshafil@pm.me>
2022-08-20 19:49:51 -0400

    pow: Update CheckProofOfWork

--- src/pow.cpp
+++ src/pow.cpp
@@ -128,18 +128,21 @@ unsigned int CalculateNextWorkRequired(const CBlockIndex *pindexLast, int64_t nF
     return bnNew.GetCompact();
 }
 
-bool CheckProofOfWork(uint256 hash, unsigned int nBits, const Consensus::Params &params) {
+bool CheckProofOfWork(uint256 hash, unsigned int nBits, const Consensus::Params& params)
+{
     bool fNegative;
     bool fOverflow;
     arith_uint256 bnTarget;
+
     bnTarget.SetCompact(nBits, &fNegative, &fOverflow);
+
     // Check range
-    if (fNegative || bnTarget == 0 || fOverflow || bnTarget > UintToArith256(params.powLimit)){
+    if (fNegative || bnTarget == 0 || fOverflow || bnTarget > UintToArith256(params.powLimit))
         return false;
-    }
-        // Check proof of work matches claimed amount
-        if (UintToArith256(hash) > bnTarget){
-           return error("CheckProofOfWork() : hash doesn't match nBits\n");
-        }
+
+    // Check proof of work matches claimed amount
+    if (UintToArith256(hash) > bnTarget)
+        return false;
+
     return true;
 }
