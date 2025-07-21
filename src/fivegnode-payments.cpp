05101c6f5f327b323c248103194200d2c5eb574a
5G <53220766+5G-Cash@users.noreply.github.com>
2025-07-21 01:21:07 -0400

    Remove obsolete FivegNode payment enforcement spork

--- src/fivegnode-payments.cpp
+++ src/fivegnode-payments.cpp
@@ -148,14 +148,8 @@ bool IsBlockPayeeValid(const CTransaction &txNew, int nBlockHeight, CAmount bloc
     if (mnpayments.IsTransactionValid(txNew, nBlockHeight, false)) {
         LogPrint("mnpayments", "IsBlockPayeeValid -- Valid fivegnode payment at height %d: %s", nBlockHeight, txNew.ToString());
         return true;
-    } else {
-        if(sporkManager.IsSporkActive(SPORK_8_FIVEGNODE_PAYMENT_ENFORCEMENT)){
-            return false;
-        } else {
-            LogPrintf("FivegNode payment enforcement is disabled, accepting block\n");
-            return true;
-        }
     }
+    return false;
 }
 
 void FillBlockPayments(CMutableTransaction &txNew, int nBlockHeight, CAmount fivegnodePayment, CTxOut &txoutFivegnodeRet, std::vector <CTxOut> &voutSuperblockRet) {
